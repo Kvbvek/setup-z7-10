cf8cec4e07433c19990bd663f8e51f5df90f535e
Jakub Brzazgacz <jakubpr19@gmail.com>
2025-10-30 21:54:55 +0100

    Added python scripts to be included in linux build

--- sw/arm/meta-zybo-agh/recipes-misc/data-udp-tx/files/data-udp-tx.py
+++ sw/arm/meta-zybo-agh/recipes-misc/data-udp-tx/files/data-udp-tx.py
@@ -4,10 +4,10 @@ import os, mmap, struct, time, socket
 # -----------------------------
 # DMA and DDR configuration
 # -----------------------------
-UIO_DMA = "/dev/uio0"
+DMA_BASE = 0x40400000
 MAP_SIZE = 0x10000
 BUF_ADDR = 0x0A000000
-BUF_SIZE = 0x1000
+BUF_SIZE = 262144 * 4   #1 MB
 
 S2MM_DMACR   = 0x30
 S2MM_DMASR   = 0x34
@@ -22,15 +22,14 @@ UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # -----------------------------
-# Open UIO to DMA
+# Open /dev/mem for DMA and DDR
 # -----------------------------
-fd_dma = os.open(UIO_DMA, os.O_RDWR | os.O_SYNC)
-mm_dma = mmap.mmap(fd_dma, MAP_SIZE, mmap.MAP_SHARED,
-                   mmap.PROT_READ | mmap.PROT_WRITE, offset=0)
+fd = os.open("/dev/mem", os.O_RDWR | os.O_SYNC)
 
-# Open DDR
-fd_mem = os.open("/dev/mem", os.O_RDWR | os.O_SYNC)
-mm_ddr = mmap.mmap(fd_mem, BUF_SIZE, mmap.MAP_SHARED,
+mm_dma = mmap.mmap(fd, MAP_SIZE, mmap.MAP_SHARED,
+                   mmap.PROT_READ | mmap.PROT_WRITE, offset=DMA_BASE)
+
+mm_ddr = mmap.mmap(fd, BUF_SIZE, mmap.MAP_SHARED,
                    mmap.PROT_READ | mmap.PROT_WRITE, offset=BUF_ADDR)
 
 def dma_wr(off, val):
@@ -39,20 +38,22 @@ def dma_wr(off, val):
 def dma_rd(off):
     return struct.unpack_from("<I", mm_dma, off)[0]
 
+# -----------------------------
+# Run DMA
+# -----------------------------
 print("Reset DMA...")
 dma_wr(S2MM_DMACR, 0x4)   # reset
 time.sleep(0.01)
 dma_wr(S2MM_DMACR, 0x1)   # run/enable
 dma_wr(S2MM_DA, BUF_ADDR)
 
-# 100 words 4 bytes each
-transfer_len = 100 * 4
+transfer_len = 262144 * 4  # 1 MB
 dma_wr(S2MM_LENGTH, transfer_len)
 
 print("Waiting for DMA...")
 while True:
     status = dma_rd(S2MM_DMASR)
-    if status & 0x0002:  # IOC_Irq
+    if status & 0x0002:  # IOC_Irq (transfer complete)
         break
     time.sleep(0.001)
 
@@ -61,16 +62,16 @@ print("Transfer ended, sending data through UDP...")
 # -----------------------------
 # Sending data
 # -----------------------------
-buf = mm_ddr.read(transfer_len)  # read data once
+mm_ddr.seek(0)
+buf = mm_ddr.read(transfer_len)
 sock.sendto(buf, (UDP_IP, UDP_PORT))
 
-print(f"Sended {transfer_len} bytes to {UDP_IP}:{UDP_PORT}")
+print(f"Sent {transfer_len} bytes ({transfer_len/1024:.1f} KB) to {UDP_IP}:{UDP_PORT}")
 
 # -----------------------------
 # Closing
 # -----------------------------
 mm_dma.close()
 mm_ddr.close()
-os.close(fd_dma)
-os.close(fd_mem)
+os.close(fd)
 sock.close()
