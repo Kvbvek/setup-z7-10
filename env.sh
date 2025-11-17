3b6cceda089a9098e4062d64f0e52e5c8ba3e834
Jakub Brzazgacz <jakubpr19@gmail.com>
2025-11-17 16:03:44 +0100

    data alignment fix

--- env.sh
+++ env.sh
@@ -1,52 +1,36 @@
-#!/bin/bash
-
-# =========================================================
+#!/bin/bash -e
 
 XILINX_INSTALL_DIR="/tools/Xilinx"
 PETALINUX_INSTALL_DIR="$HOME"
 
 VIVADO_VERSION="2022.1"
 VITIS_VERSION="2022.1"
-PETALINUX_VERSION="2022.1"
-
-# =========================================================
+# PETALINUX_VERSION="2022.1"
 
 export ROOTDIR=$(pwd)
 
 echo "Sourcing environment settings for Xilinx tools..."
 
 VIVADO_SETTINGS_PATH="${XILINX_INSTALL_DIR}/Vivado/${VIVADO_VERSION}/settings64.sh"
-if [ -f "${VIVADO_SETTINGS_PATH}" ]; then
-    echo "  - Sourcing Vivado ${VIVADO_VERSION}..."
-    source "${VIVADO_SETTINGS_PATH}"
-else
-    echo "  - ERROR: Vivado settings file not found at ${VIVADO_SETTINGS_PATH}"
-fi
+echo "  - Sourcing Vivado ${VIVADO_VERSION}..."
+source "${VIVADO_SETTINGS_PATH}"
 
 VITIS_SETTINGS_PATH="${XILINX_INSTALL_DIR}/Vitis/${VITIS_VERSION}/settings64.sh"
-if [ -f "${VITIS_SETTINGS_PATH}" ]; then
-    echo "  - Sourcing Vitis ${VITIS_VERSION}..."
-    source "${VITIS_SETTINGS_PATH}"
-else
-    echo "  - ERROR: Vitis settings file not found at ${VITIS_SETTINGS_PATH}"
-fi
+echo "  - Sourcing Vitis ${VITIS_VERSION}..."
+source "${VITIS_SETTINGS_PATH}"
 
-PETALINUX_SETTINGS_PATH="${PETALINUX_INSTALL_DIR}/petalinux/${PETALINUX_VERSION}/settings.sh"
-if [ -f "${PETALINUX_SETTINGS_PATH}" ]; then
-    echo "  - Sourcing PetaLinux ${PETALINUX_VERSION}..."
-    source "${PETALINUX_SETTINGS_PATH}"
-else
-    echo "  - ERROR: PetaLinux settings file not found at ${PETALINUX_SETTINGS_PATH}"
-fi
+# PETALINUX_SETTINGS_PATH="${PETALINUX_INSTALL_DIR}/petalinux/${PETALINUX_VERSION}/settings.sh"
+# echo "  - Sourcing PetaLinux ${PETALINUX_VERSION}..."
+# source "${PETALINUX_SETTINGS_PATH}"
 
-cd ${ROOTDIR}/sw/arm
+cd "${ROOTDIR}/sw/arm"
 
 if [ ! -d build ]; then
 
     . poky/oe-init-build-env
 
     cat <<-EOF >> conf/local.conf
-HDF_FILE = "<../../../../hw/export/1m_check.xsa>"
+HDF_FILE = "<../../../../hw/export/zybo-agh.xsa>"
 XILINX_WITH_ESW = "xsct"
 XILINX_XSCT_VERSION = "2023.1"
 XILINX_SDK_TOOLCHAIN = "/tools/Xilinx/Vitis/2022.1"
@@ -70,4 +54,4 @@ else
     echo "'build' directory exists, skipping conf"
 fi
 
-cd ${ROOTDIR}
+cd "${ROOTDIR}"
--- sw/arm/meta-zybo-agh/recipes-misc/data-test/files/data-test.py
+++ sw/arm/meta-zybo-agh/recipes-misc/data-test/files/data-test.py
@@ -2,9 +2,13 @@
 import os, mmap, struct, time
 
 DMA_BASE = 0x40400000
+GPIO_BASE = 0x41200000       # <-- TU WPISZ SWÓJ ADRES AXI GPIO
 MAP_SIZE = 0x10000
+
 BUF_ADDR = 0x0A000000
 BUF_SIZE = 1 * 1024 * 1024  # 1 MB
+
+GPIO_DATA = 0x0             # rejestr danych GPIO
 
 S2MM_DMACR   = 0x30
 S2MM_DMASR   = 0x34
@@ -16,8 +20,12 @@ fd = os.open("/dev/mem", os.O_RDWR | os.O_SYNC)
 mm_dma = mmap.mmap(fd, MAP_SIZE, mmap.MAP_SHARED,
                    mmap.PROT_READ | mmap.PROT_WRITE, offset=DMA_BASE)
 
+mm_gpio = mmap.mmap(fd, MAP_SIZE, mmap.MAP_SHARED,
+                    mmap.PROT_READ | mmap.PROT_WRITE, offset=GPIO_BASE)
+
 mm_ddr = mmap.mmap(fd, BUF_SIZE, mmap.MAP_SHARED,
                    mmap.PROT_READ | mmap.PROT_WRITE, offset=BUF_ADDR)
+
 
 def dma_wr(off, val):
     struct.pack_into("<I", mm_dma, off, val)
@@ -25,10 +33,21 @@ def dma_wr(off, val):
 def dma_rd(off):
     return struct.unpack_from("<I", mm_dma, off)[0]
 
+def gpio_wr(val):
+    struct.pack_into("<I", mm_gpio, GPIO_DATA, val)
+
+
 print("Reset DMA...")
 dma_wr(S2MM_DMACR, 0x4)
 time.sleep(0.01)
 dma_wr(S2MM_DMACR, 0x1)
+
+# ---------------------------------------------------
+#   START na stałe = 1
+# ---------------------------------------------------
+print("Setting START = 1 on GPIO...")
+gpio_wr(1)
+# ---------------------------------------------------
 
 dma_wr(S2MM_DA, BUF_ADDR)
 transfer_len = BUF_SIZE
@@ -36,7 +55,7 @@ dma_wr(S2MM_LENGTH, transfer_len)
 
 print("Waiting for DMA...")
 while True:
-    if dma_rd(S2MM_DMASR) & 0x0002:  # IOC_Irq (transfer complete)
+    if dma_rd(S2MM_DMASR) & 0x0002:  # IOC_Irq (done)
         break
     time.sleep(0.001)
 
@@ -46,10 +65,11 @@ print("First 16 words:")
 for i in range(0, 64, 4):
     print(struct.unpack_from("<I", mm_ddr, i)[0])
 
-print("\Last 16 words:")
+print("\nLast 16 words:")
 for i in range(transfer_len - 64, transfer_len, 4):
     print(struct.unpack_from("<I", mm_ddr, i)[0])
 
 mm_dma.close()
+mm_gpio.close()
 mm_ddr.close()
 os.close(fd)
